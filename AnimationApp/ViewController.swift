50b6dcd9b7b2f871b3e51e285b7458701e0b6d9f
Alexander6Shevtsov <axrsvr@gmail.com>
2024-11-02 14:12:22 +0300

    Initial Commit

---
+++ AnimationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationApp
+//
+//  Created by Alexander Shevtsov on 02.11.2024.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
