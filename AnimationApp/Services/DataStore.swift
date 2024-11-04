bfec281c57c64635350dedbce0c6e61cf06ba1fd
Alexander6Shevtsov <axrsvr@gmail.com>
2024-11-04 20:39:01 +0300

    Finished

---
+++ AnimationApp/Services/DataStore.swift
@@ -0,0 +1,8 @@
+//
+//  DataStore.swift
+//  AnimationApp
+//
+//  Created by Alexander Shevtsov on 04.11.2024.
+//
+
+import Foundation
---
+++ AnimationApp/Services/ViewControllers/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  AnimationApp
+//
+//  Created by Alexander Shevtsov on 02.11.2024.
+//
+
+import UIKit
+import SpringAnimation
+
+final class ViewController: UIViewController {
+
+    // MARK: - IB Outlets
+    @IBOutlet var animationView: SpringView!
+    @IBOutlet var animationLabel: SpringLabel! {
+        didSet {
+            animationLabel.text = animation.description
+        }
+    }
+    
+    // MARK: - Private properties
+    private var animation = Animation.randomAnimation
+    
+    // MARK: - IB Actions
+    @IBAction func animationButtonPressed(_ sender: UIButton) {
+        animationLabel.animation = "zoomOut"
+        animation.x = 50
+        animationLabel.animate()
+        animationLabel.text = animation.description
+        
+        animationLabel.animateNext { [unowned self] in
+            animationLabel.animation = "zoomIn"
+            animationLabel.animate()
+            
+            animationView.animation = animation.name
+            animationView.curve = animation.curve
+            animationView.duration = animation.duration
+            animationView.force = animation.force
+            animationView.delay = animation.delay
+            animationView.animate()
+            
+            animation = Animation.randomAnimation
+            sender.setTitle("Run \(animation.name)", for: .normal)
+        }
+    }
+}
+
